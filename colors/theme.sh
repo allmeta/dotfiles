a7b2f64087d86e55cab330e98ec5c4703ec21323
Thomas Almestad <thomasdasd@gmail.com>
2019-05-19 23:56:59 +0200

    added all dotfiles whoop

---
+++ colors/theme.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -eu
+if [[ $(echo "dln" | grep $1) ]] 
+then
+    # cd to 
+    cd "${BASH_SOURCE[0]}"
+    case $1 in
+    "l")
+        file="./themes/light"
+        ;;
+    "d")
+        file="./themes/dark"
+        ;;
+    "n")
+        file="./themes/new"
+    esac
+
+    xcolors=""
+    kitty=""
+    dunst=""
+    
+    # cp over template to dunstrc
+    # edit it in-place there. yes?
+    dunst_file="$HOME/.config/dunst/dunstrc"
+    bls_file="$HOME/.config/betterlockscreenrc"
+    $(cp -f "./templates/dunst_replace" $dunst_file)
+    $(cp -f "./templates/betterlockscreenrc_replace" $bls_file)
+    while read line;
+    do
+        # echo $line >> kitty
+        if [[ $line != "" ]]; then
+            kitty+=$line"\n"
+            line_upper="${line^^}"
+            a="#define "$line_upper"\n"
+            xcolors+=$a
+            # replace each line in file ooof rip dude xddd
+            # need to split line on space
+            line_split=($line_upper)
+            # use sed here to replace with current color
+            $(sed -i -e "s/!${line_split[0]}/${line_split[1]}/g" $dunst_file)
+            # replace betterlockscreen
+            $(sed -i -e "s/!${line_split[0]}/${line_split[1]}/g" $bls_file)
+            
+        fi
+    done < $file
+    # replace cursorColor to cursor in kitty
+    kitty_rep="${kitty/cursorColor/cursor}"
+    # echo -e $xcolors
+    echo -e $kitty_rep > "./templates/kitty"
+    echo -e $xcolors > "./templates/xcolors"
+    $(xrdb -merge $HOME/.Xresources)
+    $(kitty @ set-colors --all ./templates/kitty)
+    $(killall dunst)
+    $(i3-msg restart)
+    echo "Theme switched!"
+else
+    echo "Argument must be either d, l or n"
+fi
---
+++ zshrc
@@ -0,0 +1,40 @@
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Path to your oh-my-zsh installation.
+  export ZSH="/home/allmeta/.oh-my-zsh"
+
+ZSH_THEME="refined"
+
+
+HIST_STAMPS="dd/mm/yyyy"
+
+plugins=(git fast-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh
+
+
+# You may need to manually set your language environment
+#export LANG=no_nb.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+eval $(thefuck --alias f)
+alias ranger='source ranger'
+export TERMINAL='kitty'
