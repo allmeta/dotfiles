1f2baa0d7508921c8983c9047e919b5f601d9810
allmeta <thomasdasd@gmail.com>
2020-01-10 11:02:36 +0100

    font-1 for number in weather module

--- config/polybar/openweathermap-simple.sh
+++ config/polybar/openweathermap-simple.sh
@@ -54,5 +54,5 @@ if [ -n "$weather" ]; then
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
     weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
-    echo "$(get_icon "$weather_icon")" "$weather_temp$SYMBOL"
+    echo "$(get_icon "$weather_icon")" "%{T1}$weather_temp$SYMBOL%{T-}"
 fi
