dce75f471dbf977046f50cb28148c812ec8bbd98
allmeta <thomasdasd@gmail.com>
2020-02-03 22:41:44 +0100

    lpass_menu supports login

--- dots/i3/.config/i3/lpass_menu.sh
+++ dots/i3/.config/i3/lpass_menu.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-list="$(lpass ls)"
-
-if [[ "$list" == "" ]]; then
-	notify-send "lpass" "not logged in"
-else
-	item=`lpass ls | awk -F '[/[]' '{print $(NF-1)}' | rofi -dmenu -i -p 'Select item'`
-
-	if [ $item ]; then
-		lpass show -c --password $item
-		notify-send "$item" "Password copied"
-	fi
+if [[ "$(lpass ls)" == "" ]]; then
+	# not logged in
+	ans=$(lpass login $(cat ~/.email))
+	[[ "$ans" == "" ]] && exit 1
 fi
 
+item=`lpass ls | awk -F '[/[]' '{print $(NF-1)}' | rofi -dmenu -i -p 'Select item'`
 
-
+if [ $item ]; then
+		lpass show -c --password $item
+		notify-send "$item" "Password copied"
+fi
