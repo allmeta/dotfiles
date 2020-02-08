4a36235a8f4d49f7d624e7f4e2e89072f1296aa8
allmeta <thomasdasd@gmail.com>
2020-02-08 16:02:49 +0100

    openrc-changes

--- dots/bin/.local/bin/rofr.sh
+++ dots/bin/.local/bin/rofr.sh
@@ -3,8 +3,8 @@ ANS=$(echo " Lock| Logout| Reboot| Shutdown" | \
 		-hide-scrollbar -eh 1 -line-padding 4 -padding 50 -lines 4)
 case "$ANS" in 
 	*Lock) betterlockscreen -l ;;
-	*Logout) loginctl terminate-session $(loginctl session-status | head -n 1 | awk '{print $1}') ;;
-	*Reboot) systemctl reboot ;;
-	*Shutdown) systemctl poweroff
+	*Logout) i3-msg exit ;;
+	*Reboot) openrc-shutdown -r 0  ;;
+	*Shutdown) openrc-shutdown -p 0
 esac
 
--- dots/zprofile/.zprofile
+++ dots/zprofile/.zprofile
@@ -1,6 +1,6 @@
 
 export VISUAL=vim
 export TERMINAL=kitty
-if systemctl -q is-active graphical.target && [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+  if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
   exec startx
 fi
