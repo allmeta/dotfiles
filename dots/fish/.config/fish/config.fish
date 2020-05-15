c002a94e7ef980477a4adf95b1f006f799eabc84
allmeta <thomasdasd@gmail.com>
2020-05-16 00:16:04 +0200

    add fish

---
+++ dots/fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+alias dockem='docker run -it --rm --volume "(pwd):/home/docker/src" --workdir     "/home/docker/src" portoleks/in5570v20:latest'
+alias gl="git pull"
+alias gp="git push"
+alias gc="git commit -m"
+alias gco="git checkout"
+alias glo="git log"
+alias gst="git status"
+alias gd="git diff"
+alias ga="git add"
+alias gb="git branch"
+alias gsta="git stash push"
