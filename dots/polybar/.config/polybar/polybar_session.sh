df7cc95abc9a9328f32168f62abac0d753d02422
allmeta <thomasdasd@gmail.com>
2021-05-17 01:54:12 +0200

    useless polybar rice

--- dots/polybar/.config/polybar/polybar_session.sh
+++ dots/polybar/.config/polybar/polybar_session.sh
@@ -6,9 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# wait for pulseaudio xd
-
-until pgrep -x pulseaudio >/dev/null; do sleep 1; done
-
 # Launch polybar
 polybar -c ~/.config/polybar/config ${1:-i3} 2>~/polybar-log &
