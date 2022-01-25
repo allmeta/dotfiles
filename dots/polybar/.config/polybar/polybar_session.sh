722cb3b03c24966f4259920f6a91b71f2694eac4
allmeta <thomasdasd@gmail.com>
2022-01-25 13:13:13 +0100

    polybar: wlan env and gruvbox accent

--- dots/polybar/.config/polybar/polybar_session.sh
+++ dots/polybar/.config/polybar/polybar_session.sh
@@ -6,10 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# ENV
+export WLAN="wlp7s0"
+
 # Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c ~/.config/polybar/config ${1:-i3} 2>~/polybar-log &
+    export MONITOR=$m
+    polybar -c ~/.config/polybar/config ${1:-i3} 2>~/polybar-log &
   done
 else
 	polybar -c ~/.config/polybar/config ${1:-i3} 2>~/polybar-log &
