815621e943898786547f2f1b09d2fb7577ef0719
allmeta <thomasdasd@gmail.com>
2022-11-02 00:18:25 +0100

    polybar tray pos and calendar

--- dots/polybar/.config/polybar/polybar_session.sh
+++ dots/polybar/.config/polybar/polybar_session.sh
@@ -8,12 +8,14 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # ENV
 export WLAN="wlp7s0"
+export TRAY_POS="right"
 
 # Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     export MONITOR=$m
     polybar -c ~/.config/polybar/config ${1:-i3} 2>~/.polybar-log &
+    export -n TRAY_POS # remove TRAY_POS after first monitor, so the tray doesnt conflict on multi monitor
   done
 else
   polybar -c ~/.config/polybar/config ${1:-i3} 2>~/.polybar-log &
